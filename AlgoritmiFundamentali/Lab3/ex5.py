0c0b19ae96aeb48df7ebdde467a74451ec9aa55f
PaulVLAD22 <paulvlad34@gmail.com>
2021-01-23 14:37:58 +0200

    k

--- AlgoritmiFundamentali/Lab3/ex5.py
+++ AlgoritmiFundamentali/Lab3/ex5.py
@@ -16,19 +16,46 @@ def cmp(p1):
     return graf[p1[0]][p1[1]]
 def kruskal(n):
     culoare = [i for i in range(n + 1)]
-    acpm=[[] for i in range (n+1)]
+    acpm=[]
     tata_acpm=[0 for i in range (n+1)]
     for p in muchii_sortate:
         if (culoare[p[0]]!=culoare[p[1]]):
-            acpm[p[0]].append(p[1])
+            acpm.append((p[0],p[1]))
             tata_acpm[p[1]]=p[0]
             deSchimbat=culoare[p[0]]
             for i in range (1,n+1):
                 if (culoare[i]==deSchimbat):
                     culoare[i]=culoare[p[1]]
-            print(culoare)
     return tata_acpm,acpm
 
+def costAPCM(apcm):
+    cost=0
+    for p in apcm:
+        cost+=graf[p[0]][p[1]]
+    return cost
+
+def dfs(s):
+    if (L.get(s)!=None):
+        for x in L[s]:
+            if (viz[x]==0 and not(x in blacks)):
+                whites.remove(x)
+                viz[x]=1
+                greys.append(x)
+                tata[x]=s
+                d[x]=d[s]+1
+
+                dfs(x)
+            elif (viz[x]==1 and not(x in blacks)):
+                print(s,x,blacks)
+                print(tata[1:])
+                nod_actual=s
+                while (nod_actual!=tata[x]):
+                    circuit.append(nod_actual)
+                    nod_actual=tata[nod_actual]
+
+    blacks.append(s)
+    if(s in greys):
+        greys.remove(s)
 
 
 graf,muchii_sortate,n,m=readmat()
@@ -36,6 +63,65 @@ muchii_sortate=(sorted(muchii_sortate,key=cmp))
 print(muchii_sortate)
 
 tata_acpm,acpm=kruskal(n)
-
+print(acpm)
 # CONTINUA PROBLEMA
+
+noua_muchie=(3,5,4)
+
+graf[noua_muchie[0]][noua_muchie[1]]=noua_muchie[2]
+muchii_sortate.append((noua_muchie[0],noua_muchie[1]))
+muchii_sortate=(sorted(muchii_sortate,key=cmp))
+
+
+tata_acpm2,acpm2=kruskal(n)
+
+print(acpm2)
+
+#a)
+acpm.append((noua_muchie[0],noua_muchie[1]))
+
+L={}
+for p in acpm:
+    if (L.get(p[0])==None):
+        L[p[0]]=[p[1]]
+    else:
+        L[p[0]].append(p[1])
+s=1
+viz = [0 for i in range(n + 1)]
+tata = [0 for i in range(n + 1)]
+d = [0 for i in range(n + 1)]
+whites=[i for i in range (1,n+1)]
+whites.remove(s)
+viz[s]=1
+greys=[s]
+blacks=[]
+circuit=[]
+dfs(s)
+
+while (whites):
+    nod=whites.pop()
+    viz[nod]=1
+    greys.append(nod)
+    tata[nod]=-1
+    dfs(nod)
+print("CICUITUL:")
+print (circuit)
+maxim=0
+ma1=0
+m2=0
+for x in circuit:
+    for y in circuit:
+        if (graf[x][y]>maxim):
+            maxim=graf[x][y]
+            m1=x
+            m2=y
+
+print(maxim,m1,m2)
+
+
+
+#b)
+print(costAPCM(acpm),costAPCM(acpm2))
+
+
 
--- AlgoritmiFundamentali/Lab4/drumuriCritice.py
+++ AlgoritmiFundamentali/Lab4/drumuriCritice.py
@@ -1,1 +1,70 @@
-#fa-l
+from _collections import deque
+
+def read1(oriented = False, filename = "ex1.txt"):
+    with open(filename, "r") as f:
+        n=int(f.readline().split()[0])
+        precede = [[] for i in range(n + 1)]
+        durata=[int(x) for x in f.readline().split()]
+        gradeInt=[0 for i in range (n+1)]
+        m=int(f.readline().split()[0])
+        for i in range (m):
+            x,y = [int(i) for i in f.readline().split()]
+            precede[x].append(y)
+            gradeInt[x]+=1
+    return n,precede,durata,gradeInt,m
+
+
+
+def drumuriCritice():
+    global q, graf, n, m, gradeInt, drumCritic, durata, precede
+    inProcess=[0 for i in range (n+1)]# activitate inceputa
+
+    previousTask=[0 for i in range (n+1)] #task ul anterior
+    earliestEndTime =[1000 for i in range (n+1)] #cel mai devreme terminat un task
+    projectDuration=0 # ce se cere
+
+
+    lastTask=0
+    nod=0
+    while (q):
+        nod=q.pop()
+        inProcess[nod]=True
+        for i in range (len(precede[nod])):
+            nod_urm = precede[nod][i]
+            if (not inProcess[nod_urm]):
+                gradeInt[nod_urm]-=1
+                if (earliestEndTime[nod_urm]<durata[nod_urm]+earliestEndTime[nod]):
+                    previousTask[nod_urm]=nod
+                    earliestEndTime[nod_urm]=durata[nod_urm]+earliestEndTime[nod]
+                if (gradeInt[nod_urm]):
+                    q.append(nod_urm)
+
+    for i in range (1,n+1):
+        if (projectDuration<earliestEndTime[i]):
+            projectDuration=earliestEndTime[i]
+            lastTask=i
+    nod =lastTask
+    while (nod):
+        drumCritic.append(nod)
+        nod=previousTask[nod]
+    return precede,inProcess,durata,previousTask,earliestEndTime,projectDuration
+
+
+
+
+
+n,precede,durata,gradeInt,m=read1()
+q=deque()
+
+for i in range (1,n+1):
+    q.append(i)
+drumCritic=[]
+
+precede,inProcess,durata,previousTask,earliestEndTime,projectDuration = drumuriCritice()
+
+print(projectDuration)
+
+for i in range (len(drumCritic)-1,0):
+    print(drumCritic[i]+" ")
+
+
